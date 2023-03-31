3e9573aed25c673cb4c64b4d1dea76c9a5582602
davincidreams <lisamegawatts@gmail.com>
2023-03-31 06:32:31 -0400

    header animation and text

--- src/viewport/header/index.js
+++ src/viewport/header/index.js
@@ -9,7 +9,7 @@ const Header = () => {
     <Fragment>
       <div className="header_top">
         <nav className="navbar navbar-expand-lg">
-          <div className="container wow fadeInUp animated" data-wow-duration="1000ms" data-wow-delay="600ms">
+          <div className="container wow fadeInUp animated" data-wow-duration="300ms" data-wow-delay="250ms">
             <div className="col-sm-2">
               <a className="brand" href="Home.html">
                 <img src={logo} className="img-responsive" alt="logo" />
